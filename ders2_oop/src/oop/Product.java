3db9003786f531ce16690e1828ea2962e8696d96
Murat Iyigun <miyigun@hotmail.com>
2022-09-23 21:13:52 +0300

    First commit

---
+++ ders2_oop/src/oop/Product.java
@@ -0,0 +1,42 @@
+package oop;
+
+public class Product {
+    public String name;
+    public double unitPrice;
+    public double discount;
+    public String imageUrl;
+    public int unitInStock;
+    
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public int getUnitInStock() {
+		return unitInStock;
+	}
+	public void setUnitInStock(int unitInStock) {
+		this.unitInStock = unitInStock;
+	}
+
+    
+}
